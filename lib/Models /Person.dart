2a284c578f406f36f47f2ce1d574fd72da7ed7d0
MSCosamaShallal <osamafalah85@yahoo.com>
2021-10-04 17:06:38 +0300

    third commit

---
+++ lib/Models /Person.dart
@@ -0,0 +1,12 @@
+
+
+class Person {
+   String?  id    ;
+  String? name  ;
+  DateTime? updateTime;
+  double? height  ;
+  String? imageUrl;
+  Person(this.id , this.name, this.updateTime , this.height , this.imageUrl);
+
+}
+
--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:untitled2/Models%20/Person.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+ // const Home({Key? key}) : super(key: key);
+
+ Person person1 = Person(id, name, updateTime, height, imageUrl);
 
   @override
   Widget build(BuildContext context) {
