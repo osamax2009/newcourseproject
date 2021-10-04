e880255cc9023f5941e1d7ef02fb0e38dc07e65a
MSCosamaShallal <osamafalah85@yahoo.com>
2021-10-04 17:41:37 +0300

    third commit

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -4,10 +4,14 @@ import 'package:untitled2/Models%20/Person.dart';
 class Home extends StatelessWidget {
  // const Home({Key? key}) : super(key: key);
 
-  var myList = [
+  List<Person> myList = [
+    Person("45543543", "ali", DateTime.now(), 188.0 , "https://i.pinimg.com/236x/1f/25/5d/1f255d7f9cf3afe7cd9cd97626d08fbf.jpg"),
+    Person("543543f", "noor", DateTime.now(), 139.0 , "https://i.pinimg.com/236x/1f/25/5d/1f255d7f9cf3afe7cd9cd97626d08fbf.jpg"),
+    Person("54354d3f", "mohmmed", DateTime.now(), 170.0 , "https://i.pinimg.com/236x/1f/25/5d/1f255d7f9cf3afe7cd9cd97626d08fbf.jpg"),
     Person("45543543", "ali", DateTime.now(), 188.0 , "https://i.pinimg.com/236x/1f/25/5d/1f255d7f9cf3afe7cd9cd97626d08fbf.jpg"),
     Person("543543f", "noor", DateTime.now(), 139.0 , "https://i.pinimg.com/236x/1f/25/5d/1f255d7f9cf3afe7cd9cd97626d08fbf.jpg"),
     Person("54354d3f", "mohmmed", DateTime.now(), 170.0 , "https://i.pinimg.com/236x/1f/25/5d/1f255d7f9cf3afe7cd9cd97626d08fbf.jpg")
+
   ];
 
   @override
@@ -20,13 +24,9 @@ class Home extends StatelessWidget {
         width: double.infinity,
         child: Column(
         children: [
-          Text(
-            "osama",
-            style: new TextStyle(fontSize:36.0,
-                color: const Color(0xFFe10f0f),
-                fontWeight: FontWeight.w200,
-                fontFamily: "Roboto"),
-          ),
+          ...myList.map(
+                  (item) => Text(item.name ??"")
+          ).toList()
         ],
         ),
       ),
