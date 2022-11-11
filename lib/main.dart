95cd7a15aed43055816a9149a66ef231e74d1f40
MSCosamaShallal <osamafalah85@yahoo.com>
2022-11-11 17:49:49 +0300

    new Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:untitled2/ProductList.dart';
 
 import 'HomeScreen.dart';
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:HomeScreen() ,
+      home: ProductList() ,
     );
   }
 }
--- lib/models/Product.dart
+++ lib/models/Product.dart
@@ -4,7 +4,7 @@ class Product {
   final String  description;
   final double  price ;
   final String imageUrl ;
-  final bool faviorte ;
+  final bool favorite ;
 
   Product({
    required this.id,
@@ -12,7 +12,7 @@ class Product {
     required this.description,
     required this.price,
     required   this.imageUrl,
-    this.faviorte = false
+    this.favorite = false
   });
 
 
