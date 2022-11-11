7511b3d45418d1254a00616842c8e97764064609
MSCosamaShallal <osamafalah85@yahoo.com>
2022-11-11 17:23:55 +0300

    new Commit

--- lib/ProductList.dart
+++ lib/ProductList.dart
@@ -5,7 +5,7 @@ import 'package:untitled2/models/Product.dart';
 class ProductList extends StatelessWidget {
    ProductList({Key? key}) : super(key: key);
 
-  Product product  = Product("22","shirt","djlsdfjlkds",54,"nfljksdnfjds ") ;
+
 
   @override
   Widget build(BuildContext context) {
